50fb106e99a6949a14b28095da447e7b135c7693
rlecheta@livetouch.com.br <livetouch@2013>
2015-10-22 11:50:09 -0200

    OK

--- carros/Passo13-Configuracoes/Carros/app/src/main/java/br/com/livroandroid/carros/activity/prefs/ConfiguracoesV11Activivity.java
+++ carros/Passo13-Configuracoes/Carros/app/src/main/java/br/com/livroandroid/carros/activity/prefs/ConfiguracoesV11Activivity.java
@@ -2,25 +2,33 @@ package br.com.livroandroid.carros.activity.prefs;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import br.com.livroandroid.carros.R;
+import br.com.livroandroid.carros.activity.BaseActivity;
+import livroandroid.lib.utils.AndroidUtils;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class ConfiguracoesV11Activivity extends Activity {
+public class ConfiguracoesV11Activivity extends BaseActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_prefs);
 
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new PrefsFragment())
-                .commit();
+        setUpToolbar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if(savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fragLayout, new PrefsFragment())
+                    .commit();
+        }
 
         Toast.makeText(this, " isCheckPushOn: " + PrefsUtils.isCheckPushOn(this), Toast.LENGTH_SHORT).show();
     }
